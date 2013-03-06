babac27ad708b16ae9ec077085af8bb1af389518
teresa rivera <riverast012@gmail.com>
2013-03-06 16:09:34 -0600

    Cambios en almacenamiento

--- js/API/almacenamiento.js
+++ js/API/almacenamiento.js
@@ -1,40 +1,12 @@
 // Almacenamiento
 
-function accesoBD(nombre,tamano){
-	//Acceso a la Base de datos Local
-	var db = window.openDatabase(nombre, //Nombre de la BD
-	"1.0",  //Version de la BD
-	nombre, //Nombre a mostrar
-	tamano); //Tamaño de la BD que se piensa utilizar
-	
-	return bd;
-	}
-	
-//Se quito el codigo de la función y se envio a archivos.js	
-
-	
-function crearTabla(tx,bd,campos) {
-     tx.executeSql('DROP TABLE IF EXISTS '+bd+'');
-	 
-     tx.executeSql('CREATE TABLE IF NOT EXISTS '+bd+' ('+campos+')');
+function iniciarBD(){
+	vardb = window.openDatabase("Database","1.0","HotelR", 200000);
+	db.transaction(function(tx){
+		ts.executeSql('CREATE TABLE IF NOT EXIST historial(hId unique, fecha, habitaciones, personas, estancia');
+	},function(err){
+		alert(err.code);
+		},function(){
+		alert('ok');
+		});
 }
-
-
-	
-function almacenarBD(nombre,valor){
-	
-        window.localStorage.setItem(nombre, valor);
-	}
-	
-function obtenerBD(nombre){
-	
-        window.localStorage.setItem(nombre);
-	}
-	
-function isLogin(){
-	if(obtenerBD('nombre')!=undefined && obtenerBD('dispId')!=undefined)
-		pgAlert(nombre,"Ok");
-	else
-		pgAlert(nombre,"No")
-	
-	}
