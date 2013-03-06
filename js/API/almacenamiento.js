a7672f13c02a0be8e4e2f7397aca4a2875c71ba6
teresa rivera <riverast012@gmail.com>
2013-03-06 15:32:26 -0600

    logos

---
+++ js/API/almacenamiento.js
@@ -0,0 +1,40 @@
+// Almacenamiento
+
+function accesoBD(nombre,tamano){
+	//Acceso a la Base de datos Local
+	var db = window.openDatabase(nombre, //Nombre de la BD
+	"1.0",  //Version de la BD
+	nombre, //Nombre a mostrar
+	tamano); //Tamaño de la BD que se piensa utilizar
+	
+	return bd;
+	}
+	
+	
+
+	
+function crearTabla(tx,bd,campos) {
+     tx.executeSql('DROP TABLE IF EXISTS '+bd+'');
+	 
+     tx.executeSql('CREATE TABLE IF NOT EXISTS '+bd+' ('+campos+')');
+}
+
+
+	
+function almacenarBD(nombre,valor){
+	
+        window.localStorage.setItem(nombre, valor);
+	}
+	
+function obtenerBD(nombre){
+	
+        window.localStorage.setItem(nombre);
+	}
+	
+function isLogin(){
+	if(obtenerBD('nombre')!=undefined && obtenerBD('dispId')!=undefined)
+		pgAlert(nombre,"Ok");
+	else
+		pgAlert(nombre,"No")
+	
+	}
--- js/API/archivos.js
+++ js/API/archivos.js
@@ -1,4 +1,5 @@
 //Archivos
+
 function subirArchivo(ruta){
 	var ft = new FileTransfer();
 
@@ -6,7 +7,13 @@ function subirArchivo(ruta){
 		"http://igitsoft.com/pgtest.php",
 		function(result) {
 			pgAlert('Imagen subida',result.responseCode+'\n'+result.bytesSent);
+			
+			function transacciones(bd,ejecuciones){
+		db.transaction(accesoBD('test',100000),function(tx){
+			crearTabla(tx,'Reserva','rId,fecha,habitaciones,personas,estancia')}, alert("Error processing SQL: "+err), alert("success!"));
+	}
 		},
+	
 		function(error) {
 			pgAlert('Error al subir imagen',error.code);
 		},
